0782652e21e7a4b0228dac0acdaa63c550993510
Kevin O'Toole <kotoole@onshape.com>
2015-03-31 23:00:14 -0400

    1.2 6-10V

--- Lab2/collecting_data_lab2.py
+++ Lab2/collecting_data_lab2.py
@@ -6,7 +6,10 @@ if platform.system() == 'Linux':
 elif platform.system() == 'Darwin':
     data = serial.Serial('/dev/tty.usbmodem1451')
 
-file_ = open('data/Part1_2_5V.csv', 'w')
+file_ = open('data/Part1_2_10V.csv', 'w')
+wait = time.time() + 3
+while time.time() < wait:
+    pass
 stop = time.time() + 3
 while time.time() < stop:
 	a = data.readline()
