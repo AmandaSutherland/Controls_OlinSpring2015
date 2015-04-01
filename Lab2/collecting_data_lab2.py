cef80d05b2691ce842546ecdc2396e03d0fd0ba2
Kevin O'Toole <kotoole@onshape.com>
2015-03-31 22:53:01 -0400

    adding 1.2 5V

--- Lab2/collecting_data_lab2.py
+++ Lab2/collecting_data_lab2.py
@@ -1,6 +1,13 @@
 import serial 
-data = serial.Serial('/dev/ttyACM0', 9600)
-file_ = open('data/touch_test.csv', 'w')
-while(1):
+import platform
+import time
+if platform.system() == 'Linux':
+    data = serial.Serial('/dev/ttyACM0', 9600)
+elif platform.system() == 'Darwin':
+    data = serial.Serial('/dev/tty.usbmodem1451')
+
+file_ = open('data/Part1_2_5V.csv', 'w')
+stop = time.time() + 3
+while time.time() < stop:
 	a = data.readline()
-	file_.write(a) 
+	file_.write(a)
