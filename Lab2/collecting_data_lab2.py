c13e23499d2f5aee264010303386d9c23f45f996
Kevin O'Toole <kotoole@onshape.com>
2015-04-01 19:17:28 -0400

    adding data for part 1.2, with 2-second sample rates

--- Lab2/collecting_data_lab2.py
+++ Lab2/collecting_data_lab2.py
@@ -6,11 +6,11 @@ if platform.system() == 'Linux':
 elif platform.system() == 'Darwin':
     data = serial.Serial('/dev/tty.usbmodem1451')
 
-file_ = open('data/Part2_3_600mA.csv', 'w')
-wait = time.time() + 3
+file_ = open('data/Part2_1_8V.csv', 'w')
+wait = time.time() + 1
 while time.time() < wait:
     pass
-stop = time.time() + 1
+stop = time.time() + 6
 while time.time() < stop:
 	a = data.readline()
 	file_.write(a)
